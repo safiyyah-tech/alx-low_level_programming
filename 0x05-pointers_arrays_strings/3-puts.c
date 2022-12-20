91f55251aff006da1da7f6ddea9078ce7fdbeb72
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2022-12-20 13:03:28 -0800

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line and thena standout
+ * _puts - prints a string, followed by a new line and then a stdout
  * @str: the string to print
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(*str++);
+		_putchar(str[index]);
 	}
 	_putchar('\n');
 }
