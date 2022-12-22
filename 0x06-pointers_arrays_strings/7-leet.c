5277e991e55ef2bfaac78c4e152af0b939b46abd
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2022-12-22 14:35:20 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+
+/**
+ * leet - encodes into 1337speak
+ * @n: input value
+ * Return: n
+ */
+char *leet(char *n)
+{
+	int i, j;
+
+	char str1[] = "aAeEoOtTlL";
+
+	char str2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == str1[j])
+			{
+				n[i] = str2[j];
+			}
+		}
+	}
+	return (n);
+}
