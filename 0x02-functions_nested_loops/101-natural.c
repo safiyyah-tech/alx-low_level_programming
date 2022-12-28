d948df3ef12dc86e1e31a8439f6457cc7d4bbc84
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2022-12-28 14:16:31 -0800

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the multiles of 3, 5 to 1024
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	intt i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+
+	return (0);
+}
