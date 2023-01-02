0558bc8f057a1ac95fe5fe28c2c5aa723e70fece
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2023-01-02 13:52:59 -0800

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a character
+ * @p: pointer to pointer
+ * @pc: pointer to char
+ * Return: void
+ */
+void set_string(char **p, char *pc)
+{
+	*p = pc;
+}
