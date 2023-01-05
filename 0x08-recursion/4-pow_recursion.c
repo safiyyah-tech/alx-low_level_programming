2e05a65ba852fd111d54045d63799408ecabc1cc
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2023-01-05 13:09:01 -0800

    4-recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the value
+ * @y: the raised valuse
+ * Return: the value of the raised value
+ */
+int _pow_recursion(intx, int y)
+{
+	if (y < 0)
+		return (-1);
+	if ( y == 0)
+		return (1);
+	return (x * _poe_recursion(x, y - 1));
+}
