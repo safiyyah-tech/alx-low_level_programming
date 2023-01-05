645911f3c23d31304da6c1c56ad3faf2e834cfa1
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2023-01-05 13:10:54 -0800

    4-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,11 +6,11 @@
  * @y: the raised valuse
  * Return: the value of the raised value
  */
-int _pow_recursion(intx, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if ( y == 0)
+	if (y == 0)
 		return (1);
-	return (x * _poe_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
