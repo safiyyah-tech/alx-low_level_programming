ff872343317122f40b899f91ad3747f48b45dccd
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2023-01-16 05:14:29 -0800

    0-create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = malloc(size *sizeof(char));
+	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (x = 0; x < size; x++)
