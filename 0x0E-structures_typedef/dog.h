7a0fe9de87d1c4ff3f119b86d6c5b5d3724462bb
Safiyyah Mahmud Abdullah <msafiyyah1@gmail.com>
2023-01-20 05:43:17 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - dog attributes
+ * @name: the dog's name
+ * @age: the dog's age
+ * @ownwer: the dog's owner
+ * Description: the dog's attributes
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+
+/**
+ * dog_t - typedef for the dog'd structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
